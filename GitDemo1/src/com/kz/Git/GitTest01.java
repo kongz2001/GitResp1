81a787d7e00eee3ee3bcd6740e0d033a86fb1bec
kongz <2312525064@qq.com>
2021-09-18 16:48:48 +0800

    在GitTest01中添加了age ,name 属性

--- GitDemo1/src/com/kz/Git/GitTest01.java
+++ GitDemo1/src/com/kz/Git/GitTest01.java
@@ -1,8 +1,12 @@
 package com.kz.Git;
 
+import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
+
 /**
  * @Auther: kongz
  * @verson:1.0
  */
 public class GitTest01 {
+    private int age;
+    private String name;
 }
